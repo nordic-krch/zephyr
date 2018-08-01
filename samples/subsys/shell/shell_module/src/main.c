f9df0320e1ffca11ff285b3f4315c12a4832043a
Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>
2018-08-01 14:03:56 +0200

    samples: subsys: shell: Port sample to new shell
    Signed-off-by: Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>

--- samples/subsys/shell/shell_module/src/main.c
+++ samples/subsys/shell/shell_module/src/main.c
@@ -6,53 +6,56 @@
 
 #include <zephyr.h>
 #include <misc/printk.h>
-#include <shell/shell.h>
+#include <shell/cli.h>
+#include <shell/shell_uart.h>
 #include <version.h>
+#include <logging/log.h>
 
-static int shell_cmd_ping(int argc, char *argv[])
+SHELL_UART_DEFINE(shell_transport_uart);
+SHELL_DEFINE(uart_shell, "uart:~$ ", &shell_transport_uart, '\r', 10);
+
+static void cmd_demo_ping(const struct shell *shell, size_t argc, char **argv)
 {
 	ARG_UNUSED(argc);
 	ARG_UNUSED(argv);
 
-	printk("pong\n");
-
-	return 0;
+	shell_fprintf(shell, SHELL_NORMAL, "pong\r\n");
 }
 
-static int shell_cmd_params(int argc, char *argv[])
+static void cmd_demo_params(const struct shell *shell, size_t argc, char **argv)
 {
 	int cnt;
 
-	printk("argc = %d\n", argc);
+	shell_fprintf(shell, SHELL_NORMAL, "argc = %d\r\n", argc);
 	for (cnt = 0; cnt < argc; cnt++) {
-		printk("  argv[%d] = %s\n", cnt, argv[cnt]);
+		shell_fprintf(shell, SHELL_NORMAL,
+				"  argv[%d] = %s\r\n", cnt, argv[cnt]);
 	}
-	return 0;
 }
 
-#define SHELL_CMD_VERSION "version"
-static int shell_cmd_version(int argc, char *argv[])
+static void cmd_version(const struct shell *shell, size_t argc, char **argv)
 {
 	ARG_UNUSED(argc);
 	ARG_UNUSED(argv);
 
-	printk("Zephyr version %s\n", KERNEL_VERSION_STRING);
-	return 0;
+	shell_fprintf(shell, SHELL_NORMAL,
+		      "Zephyr version %s\r\n", KERNEL_VERSION_STRING);
 }
 
-SHELL_REGISTER_COMMAND(SHELL_CMD_VERSION, shell_cmd_version,
-		       "Show kernel version");
-
-#define MY_SHELL_MODULE "sample_module"
-
-static struct shell_cmd commands[] = {
-	{ "ping", shell_cmd_ping, NULL },
-	{ "params", shell_cmd_params, "print argc" },
-	{ NULL, NULL, NULL }
+SHELL_CREATE_STATIC_SUBCMD_SET(sub_demo)
+{
+	/* Alphabetically sorted. */
+	SHELL_CMD(params, NULL, "Print params command.", cmd_demo_params),
+	SHELL_CMD(ping, NULL, "Ping command.", cmd_demo_ping),
+	SHELL_SUBCMD_SET_END /* Array terminated. */
 };
+
+SHELL_CMD_REGISTER(demo, &sub_demo, "Demo commands", cmd_version);
+
+SHELL_CMD_REGISTER(version, NULL, "Show kernel version", cmd_version);
 
 
 void main(void)
 {
-	SHELL_REGISTER(MY_SHELL_MODULE, commands);
+	(void)shell_init(&uart_shell, NULL, true, true, LOG_LEVEL_INF);
 }
