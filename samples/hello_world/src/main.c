49e3610e98ecdf80822d17d78756a288d4f80db6
Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>
2019-04-23 15:37:36 +0200

    example that shows LFRC inaccuracy
    Signed-off-by: Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -6,8 +6,33 @@
 
 #include <zephyr.h>
 #include <misc/printk.h>
+#include <soc.h>
+#include <nrf_clock.h>
 
 void main(void)
 {
 	printk("Hello World! %s\n", CONFIG_BOARD);
+
+	NRF_CLOCK->LFCLKSRC = NRF_CLOCK_LFCLK_Xtal;
+	NRF_CLOCK->TASKS_LFCLKSTART = 1;
+
+	k_busy_wait(1000000);
+
+	NRF_RTC0->TASKS_START = 1;
+
+	k_busy_wait(1000000);
+	printk("1 second Xtal: %d\n", NRF_RTC0->COUNTER);
+
+	NRF_RTC0->TASKS_STOP = 1;
+	NRF_RTC0->TASKS_CLEAR = 1;
+	NRF_CLOCK->TASKS_LFCLKSTOP= 1;
+	k_busy_wait(100000);
+	NRF_CLOCK->LFCLKSRC = NRF_CLOCK_LFCLK_RC;
+	NRF_CLOCK->TASKS_LFCLKSTART = 1;
+
+	k_busy_wait(1000000);
+
+	NRF_RTC0->TASKS_START = 1;
+	k_busy_wait(1000000);
+	printk("1 second RC: %d\n", NRF_RTC0->COUNTER);
 }
