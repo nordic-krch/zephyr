c3da4862b7578a47004421ceb944cbe865509d96
Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>
2020-06-01 15:55:28 +0200

    temp: allow custom nrf52840 reset pin
    Signed-off-by: Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>

--- boards/arm/nrf9160dk_nrf9160/nrf52840_reset.c
+++ boards/arm/nrf9160dk_nrf9160/nrf52840_reset.c
@@ -13,8 +13,8 @@
 /* Must be a pin from 17 to 23.
  * Only those can be connected to the nRF52840.
  */
-BUILD_ASSERT(RESET_PIN > 16 && RESET_PIN < 24,
-	     "Selected pin is not connected to nRF52840");
+/*BUILD_ASSERT(RESET_PIN > 16 && RESET_PIN < 24,
+	     "Selected pin is not connected to nRF52840");*/
 
 #include <logging/log.h>
 LOG_MODULE_REGISTER(nrf52_reset);
