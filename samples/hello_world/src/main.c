be32f10751e850e5f2b55267140ba596f4bb3b3e
Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>
2021-07-29 11:05:46 +0200

    timer_sync_problem

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -6,8 +6,29 @@
 
 #include <zephyr.h>
 #include <sys/printk.h>
+#include <random/rand32.h>
+
+static void timeout(struct k_timer *timer)
+{
+
+}
 
 void main(void)
 {
+	int cnt = 0;
+	int i = 0;
+	struct k_timer timer;
+
+	k_timer_init(&timer, timeout, NULL);
+
 	printk("Hello World! %s\n", CONFIG_BOARD);
+	while (1) {
+		printk("%d\n", cnt++);
+		k_timer_start(&timer, K_USEC(100), K_NO_WAIT);
+		k_busy_wait(80 + i);
+		i++;
+		if (i > 30) i = 0;
+
+		k_timer_status_sync(&timer);
+	}
 }
