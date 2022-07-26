e253a497c432ca3f39f3799c91cc4302445cf1d3
Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>
2022-07-26 16:06:03 +0200

    Attempt to reproduce logging failure

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -5,8 +5,22 @@
  */
 
 #include <zephyr/zephyr.h>
+#include <logging/log.h>
+
+LOG_MODULE_REGISTER(app);
+static void timeout(struct k_timer *timer)
+{
+	LOG_ERR("interrupt");
+}
+
+K_TIMER_DEFINE(timer, timeout, NULL);
 
 void main(void)
 {
-	printk("Hello World! %s\n", CONFIG_BOARD);
+	k_timer_start(&timer, K_MSEC(5), K_MSEC(5));
+
+	while (1) {
+		LOG_WRN("thread");
+		k_msleep(10);
+	}
 }
